a853c55d2f8239153043f2e1274a6684e5ae9322
Yolis <yolisisabel28@gmail.com>
2023-01-27 06:19:56 -0800

    created a new elem checklist and added attributes

--- recipe2023/index.js
+++ recipe2023/index.js
@@ -7,12 +7,20 @@ button.addEventListener("click", () => {
 
 //DOM ON CHECKLIST
 
-let fetch = document.getElementById(checklist);
-//let's create a list item and store in a variable
-let newList = document.createElement("li");
+// let fetch = document.getElementById(checklist);
+// //let's create a list item and store in a variable
+// let newList = document.createElement("li");
 
-//lets update the text content of that list item
-newListItem.textContent = "Sirracha";
+// //lets update the text content of that list item
+// newListItem.textContent = "Sirracha";
 
-//Add that ul as list child
-document.querySelector("ul").appendChild(checklist);
+// //Add that ul as list child
+// document.querySelector("ul").appendChild(checklist);
+const myDiv = document.getElementById("checklist");
+
+let checkbox = document.createElement("input");
+
+checkbox.type = "checkbox";
+checkbox.name = "name";
+checkbox.value = "value";
+checkbox.id = "id";
