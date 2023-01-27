57073e157043d78f9e097216d125e93b3656080e
Yolis <yolisisabel28@gmail.com>
2023-01-26 17:05:15 -0800

    added createelem list added text content and added queryselector appendchild for ul

--- recipe2023/index.js
+++ recipe2023/index.js
@@ -1,1 +1,16 @@
+let button = document.querySelector("button");
 
+let color = "pink";
+button.addEventListener("click", () => {
+  document.body.style.backgroundColor = "pink";
+});
+
+//DOM ON CHECKLIST
+//let's create a list item and store in a variable
+let newList = document.createElement("li");
+
+//lets update the text content of that list item
+newListItem.textContent = "Sirracha";
+
+//Add that ul as list child
+document.querySelector("ul").appendChild(newList);
