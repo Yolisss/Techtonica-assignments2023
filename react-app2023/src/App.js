df133e874810b43e01c01fcec1e86029d44e8ac9
Yolis <yolisisabel28@gmail.com>
2023-02-14 19:42:33 -0800

    built first component displaymessage structure

---
+++ react-app2023/src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import DisplayMessage from "./components/DisplayMessage";
+
+function App() {
+  return (
+    <div className="App">
+      <DisplayMessage greeting="Hello" />
+    </div>
+  );
+}
+
+export default App;
---
+++ react-app2023/src/components/DisplayMessage.js
@@ -0,0 +1,12 @@
+function Game(props) {
+  return (
+    <div>
+      <div className="card">
+        <div>Guess My Number</div>
+        <div>?</div>
+      </div>
+    </div>
+  );
+}
+
+export default Game;
