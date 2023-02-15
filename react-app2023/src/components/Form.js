800cfa7c7e051ec83c21dc108784e78b207cfc97
Yolis <yolisisabel28@gmail.com>
2023-02-14 21:57:49 -0800

    created form component

---
+++ react-app2023/src/components/Form.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Form() {
+  return <div></div>;
+}
+
+//create form
+//form element with input element inside of it
+// input element contain the user's guess
+//[guess, setGuess]= useState(""); useState will be written
+//before the return statement
+// user's guess: number type
+//onChange handles the change of input
