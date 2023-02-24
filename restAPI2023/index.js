5572e7d09a930aa7c517f6bdee8455420e9d1e9c
Yolis <yolisisabel28@gmail.com>
2023-02-23 19:59:15 -0800

    added books file added type module in packagejson and had to import in indexjs

--- restAPI2023/index.js
+++ restAPI2023/index.js
@@ -1,22 +1,35 @@
 //adding libraries of express and cors
-const express = require("express");
-const cors = require("cors");
+//you are buying the ikea furniture (comes w instructions)
+//const express = require("express");
+import express from "express";
+//const cors = require("cors");
+import cors from "cors";
+//import your books
+import BOOKS from "./books.js";
 
 //creating an instance of express. This will be the main instance running my webserver
+//instance is basically something useable
+//telling the computer to build the furniture so you can use it
 const app = express();
 
 //telling express to use cors; app represents the instance we are running
 //"hey express can you use cors"
+//security caution added to browsers that can deny JS from accessing
+//any arbitrary wesbites
+//should only make calls to the same domain name
+//
 app.use(cors());
 
-//reusable in the future or make it easy to access or update
-const PORT = 5001;
-
-//set up simple route that will handle GET request to the root
-//path of our api
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Hello, World!");
 });
+
+app.get("/api/books", (req, res) => {
+  res.json(books);
+});
+
+//reusable in the future or make it easy to access or update
+const PORT = 5003;
 
 //START instance of express
 //by default, ports are closed, in order to have it open, you need to have something
