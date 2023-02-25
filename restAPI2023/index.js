707ce6e7265b0f31e15afe7020c807d782bb040c
Yolis <yolisisabel28@gmail.com>
2023-02-25 13:39:34 -0800

    created post and delete request and added notes

--- restAPI2023/index.js
+++ restAPI2023/index.js
@@ -20,6 +20,9 @@ const app = express();
 //
 app.use(cors());
 
+//tell express to handle incoming json as part of the body
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send("Hello, World!");
 });
@@ -31,6 +34,10 @@ app.get("/api/books", (req, res) => {
 app.get("/api/books/:bookID", async (req, res) => {
   //represents the obj that will be returned with the specific
   //data we're asking for
+  //params = anytime you have a param in route
+  //ex :bookID; in order to access these values;
+  //you need to do req.params
+  //params used for searching sorting filtering etc
   let requestedBook = req.params.bookID;
   for (let book of BOOKS) {
     if (book.isbn === requestedBook) {
@@ -39,7 +46,30 @@ app.get("/api/books/:bookID", async (req, res) => {
   }
 });
 
-app.post("/api/books", (req, res) => {});
+app.post("/api/books", (req, res) => {
+  //client sending data via the body
+  //newBook will be a new obj
+  let newBook = req.body;
+
+  BOOKS.push(newBook);
+  res.json(BOOKS);
+});
+
+app.delete("/api/books", (req, res) => {
+  //query.params what comes afrer a ? in url
+  //they are key value pairs seperated by &
+  //if there is more than 1
+  //and its always written as a key=value
+  //to access those you need req.query
+  //bookID=0553120441
+  let selectedBook = req.query.bookID;
+  for (let index in BOOKS) {
+    if (BOOKS[index].isbn === selectedBook) {
+      BOOKS.splice(index, 1);
+    }
+  }
+  res.json(BOOKS);
+});
 
 //reusable in the future or make it easy to access or update
 const PORT = 5003;
