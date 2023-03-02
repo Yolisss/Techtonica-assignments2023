8b8530cad36f47c4eed7248032892997946afeb0
Yolis <yolisisabel28@gmail.com>
2023-03-01 19:31:49 -0800

    created env file and  applied api key var into api url and specified what data we wanted out of the results when returned

--- weatherapp2023/server/server.js
+++ weatherapp2023/server/server.js
@@ -2,8 +2,10 @@
 //you are buying the ikea furniture (comes w instruc)
 const express = require("express");
 const cors = require("cors");
+//loading up your env variables
+//opening .env file and loading variables into your runtime execution
 require("dotenv").config();
-const dataWeather = require("./data");
+//const dataWeather = require("./data");
 
 //creating an instance of express
 //main instance running my server
@@ -23,14 +25,58 @@ app.use(cors());
 //the body
 app.use(express.json());
 
+const loadCity = () => {
+  fetch("http://localhost:8080/api/weather")
+    .then((response) => response.json())
+    .then((result) => {
+      //console.log(result)
+      setCity(result.weather[0].name);
+      setResult(result);
+    });
+};
+
 // creates an endpoint for the route /api
 app.get("/", (req, res) => {
   res.json({ message: "Hello from My template ExpressJS" });
 });
 
 // creates an endpoint for the route /api/weather
+//https://api.openweathermap.org/data/2.5/weather?q=Watsonville&appid=KEY
+//when i create the fetch structure, i want to use this url
+// so it can match with the info from data.js
 app.get("/api/weather", (req, res) => {
-  res.json(dataWeather);
+  //recieving request. in query param we're getting city value
+  //process: is a reference to the current execution that's running your code (node)
+  //any environment var that node has access to, will be accessible via process.env
+  //exist by default in terminal
+  let key = process.env.API_KEY;
+  let city = req.query.city;
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`
+  )
+    //what code do you want to execute when that fetch is finished
+    //.then executes once the fetch has been resolved
+    //i get the respond back essentially as a string
+    //json converts string to actual object
+    //.json returns another promise
+    .then((response) => response.json())
+    .then((result) => {
+      //console.log(result)
+      result.name;
+      result.weather[0].icon;
+      result.main.temp;
+      result.main.humidity;
+      result.wind.speed;
+      //it is returning that result obj as a json response
+      //that can then be used by the front end
+      res.json({
+        name: result.name,
+        icon: result.weather[0].icon,
+        temp: result.main.temp,
+        humidity: result.main.humidity,
+        windspeed: result.wind.speed,
+      });
+    });
 });
 
 // console.log that your server is up and running
