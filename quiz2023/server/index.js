a6041d9111916cc4e7af21fa1a89ba0fde73619d
Yolis <yolisisabel28@gmail.com>
2023-03-08 18:45:01 -0800

    fixed the way i was calling the info from obj that was returned from url

--- quiz2023/server/index.js
+++ quiz2023/server/index.js
@@ -39,19 +39,18 @@ app.get("/api/game", (req, res) => {
     //i get the respond back essentially as a string
     //json converts string to actual object
     //.json returns another promise
+    //parsing =
+    //json is looking at the string response from the URL
+    //and parses(converting) the string to an obj
     .then((response) => response.json())
-    .then((result) => {
-      console.log(result);
-      result.questions;
-      result.category;
-      result.difficulty;
+    .then((data) => {
+      console.log(data);
+      data.results;
       //it is returning that result obj as a json response
       //that can then be used by the front end
       //result defining new obj from api
       res.json({
-        Question: result.questions,
-        category: result.category,
-        difficulty: result.difficulty,
+        questions: data.results,
       });
     });
 });
